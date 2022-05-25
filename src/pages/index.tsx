69aa0b7909f403e70b76d1318ee755e984859899
ricardofsdomene <100056112+ricardofsdomene@users.noreply.github.com>
2022-05-24 23:19:09 -0300

    fix: /

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -155,7 +155,7 @@ export default function Index() {
             <Image
               maxH={size.height / 2 - 10}
               maxW={size.width - 100}
-              src="https://f19c-177-54-149-41.sa.ngrok.io/images/iso-nodes.png"
+              src="https://o.remove.bg/downloads/4ae48949-1c8b-4f27-8e65-786372dc758d/3441E8F9-9521-4405-8D90-76186A769157-removebg-preview.png"
             />
           )}
         </Flex>
